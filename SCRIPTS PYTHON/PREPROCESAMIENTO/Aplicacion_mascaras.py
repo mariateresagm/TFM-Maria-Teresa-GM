24ad1ce26740aa673cbf177c722e76c630ae457b
mariateresagm <mariateresagm2001@gmail.com>
2024-09-17 14:27:05 +0200

    Update Aplicacion_mascaras.py

--- SCRIPTS PYTHON/PREPROCESAMIENTO/Aplicacion_mascaras.py
+++ SCRIPTS PYTHON/PREPROCESAMIENTO/Aplicacion_mascaras.py
@@ -9,10 +9,10 @@ from arcpy.sa import *
 import os
 
 # Definición de rutas
-rasters_originales = "ruta/a/tu/directorio/trabajo"
-mascara = "ruta/a/tu/archivo_mascara/mascara.shp"
-raster_alineacion = "ruta/a/tu/raster_alineacion/rasteralineacion.shp"
-rasters_procesados = "ruta/a/tu/directorio/salida"
+rasters_originales = "ruta/directorio/trabajo"
+mascara = "ruta/archivo_mascara/mascara.shp"
+raster_alineacion = "ruta/raster_alineacion/rasteralineacion.shp"
+rasters_procesados = "ruta/directorio/salida"
 
 # Establecimiento del entorno de trabajo
 arcpy.env.workspace = rasters_originales
