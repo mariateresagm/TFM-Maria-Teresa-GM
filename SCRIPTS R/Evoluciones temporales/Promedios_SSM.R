d39a7613188cb0ce03c7e6248a07c4392ad16b41
mariateresagm <mariateresagm2001@gmail.com>
2024-09-17 23:00:12 +0200

    Evoluciones temporales

--- SCRIPTS R/Evoluciones temporales/Promedios_SSM.R
+++ SCRIPTS R/Evoluciones temporales/Promedios_SSM.R
@@ -7,35 +7,32 @@
 library(raster)
 
 # Establecimiento del directorio de trabajo
-directorio <- "ruta/a/tu/directorio/trabajo"
+directorio <- "ruta/directorio/trabajo"
 
 # Listado de años para los que se quieren calcular sus promedios
-anos <- 2017:2023
+años <- 2017:2023
 
 # Creación de un dataframe para el almacenamiento de resultados
 resultados_df <- data.frame(
-  Ano = integer(),
+  Año = integer(),
   Media = numeric(),
   stringsAsFactors = FALSE
 )
 
 # Procesamiento por cada año
-for (ano in anos) {
+for (año in años) {
   # Construcción del nombre del archivo
-  nombre_raster <- sprintf("SSMminverano%d.tif", ano)
-  ruta_raster <- file.path(directorio, nombre_raster)
-  
+  nombre_raster <- sprintf("SSMminverano%d.tif", año)
+  ruta_raster <- file.path(directorio, nombre_raster)  
   # Verificación de la existencia del raster
   if (file.exists(ruta_raster)) {
     # Lectura del raster
-    raster_actual <- raster(ruta_raster)
-    
+    raster_actual <- raster(ruta_raster)    
     # Cálculo de promedios
-    media_valores <- mean(raster_actual[], na.rm = TRUE)
-    
+    media_valores <- mean(raster_actual[], na.rm = TRUE)    
     # Agregación de resultados al dataframe
     resultados_df <- rbind(resultados_df, data.frame(
-      Ano = ano,
+      Año = año,
       Media = media_valores,
       stringsAsFactors = FALSE
     ))
@@ -48,7 +45,7 @@ for (ano in anos) {
 print(resultados_df)
 
 # Ruta donde guardar archivo CSV con los resultados
-output_csv_path <- "ruta/a/tu/directorio/salida/promedios_ssm.csv"
+output_csv_path <- "ruta/directorio/salida/promedios_ssm.csv"
 
 # Exportación de resultados en archivo CSV
 write.csv(resultados_df, output_csv_path, row.names = FALSE)
